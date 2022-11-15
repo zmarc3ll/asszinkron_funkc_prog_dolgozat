927b2a07fcc0f14a17f640b5aae9ea0f57367cb8
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-11-15 10:14:14 +0100

    second button working

--- src/index.js
+++ src/index.js
@@ -18,11 +18,34 @@ async function showAllQuotes() {
 }
 
 async function felkover() {
+    let allQuotesArray = [];
     let response = await fetch('/src/quotes.json');
     let result = await response.json();
+    let data = result.quotes.sort(function (a, b) {
+        if (a.quote < b.quote) { return -1; }
+        if (a.quote > b.quote) { return 1; }
+        return 0;
+    });
+    for (let q of data) {
+        data.forEach(q => {
+            q.quote = q.quote.replace(' the ','<b> the </b>');
+            q.quote = q.quote.replace('The ','<b>The </b>');
+        })
+        allQuotesArray.push(q.quote);
+    }
+    let dataList = document.getElementById('dataList');
+    dataList.textContent='';
+
+    for (let q of allQuotesArray) {
+        let li = document.createElement('li');
+        li.innerHTML = q;
+        dataList.appendChild(li);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('allQuotes').addEventListener('click', () => { showAllQuotes() });
-})
+    document.getElementById('felkover').addEventListener('click', () => { felkover() });
+
+});
 

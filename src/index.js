67b5a89deecc4b3956d2a6bb8e93245a3898ba34
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-11-15 09:33:38 +0100

    first button working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-import './style.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
-import  'bootstrap/dist/css/bootstrap.css';
+async function showAllQuotes() {
 
+    let response = await fetch('/src/quotes.json');
+    let result = await response.json();
+    let data = result.quotes.sort(function (a, b) {
+        if (a.quote < b.quote) { return -1; }
+        if (a.quote > b.quote) { return 1; }
+        return 0;
+    });
+    let allQuotesList = document.getElementById('dataList');
+    allQuotesList.textContent = '';
 
+    for (let q of data) {
+        let li = document.createElement('li');
+        li.innerHTML = q.quote;
+        allQuotesList.appendChild(li);
+    }
+}
+
+async function felkover() {
+    let response = await fetch('/src/quotes.json');
+    let result = await response.json();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('allQuotes').addEventListener('click', () => { showAllQuotes() });
+})
 
